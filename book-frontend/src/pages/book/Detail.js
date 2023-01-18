f9575cd5cc09c265bb6d6f90c9d3162a54517bde
cyccc95 <cyccc95@naver.com>
2023-01-19 00:47:03 +0900

    수정삭제하기

--- book-frontend/src/pages/book/Detail.js
+++ book-frontend/src/pages/book/Detail.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const Detail = (props) => {
   const propsParam = useParams();
   const id = propsParam.id;
+
+  const navigate = useNavigate();
 
   const [book, setBook] = useState({
     id: '',
@@ -19,9 +22,33 @@ const Detail = (props) => {
       });
   }, []);
 
+  const deleteBook = () => {
+    fetch('http://localhost:8080/book/' + id, {
+      method: 'DELETE',
+    })
+      .then((res) => res.text())
+      .then((res) => {
+        if (res === 'ok') {
+          navigate('/');
+        } else {
+          alert('삭제실패');
+        }
+      });
+  };
+
+  const updateBook = () => {
+    navigate('/updateForm/' + id);
+  };
+
   return (
     <div>
       <h1>책 상세보기</h1>
+      <Button variant="warning" onClick={updateBook}>
+        수정
+      </Button>{' '}
+      <Button variant="danger" onClick={deleteBook}>
+        삭제
+      </Button>
       <hr />
       <h3>{book.author}</h3>
       <h1>{book.title}</h1>
