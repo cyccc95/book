0144aff3f3444fd934f71664c5e579b5172917b9
cyccc95 <cyccc95@naver.com>
2023-01-18 22:18:55 +0900

    Routing Page 세팅

--- book-frontend/src/App.js
+++ book-frontend/src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Detail from './pages/book/Detail';
+import Home from './pages/book/Home';
+import SaveForm from './pages/book/SaveForm';
+import UpdateForm from './pages/book/UpdateForm';
+import LoginForm from './pages/user/LoginForm';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" exact={true} element={<Home />} />
+          <Route path="/saveForm" exact={true} element={<SaveForm />} />
+          <Route path="/book/:id" exact={true} element={<Detail />} />
+          <Route path="/loginForm" exact={true} element={<LoginForm />} />
+          <Route path="/joinForm" exact={true} element={<LoginForm />} />
+          <Route path="/updateForm/:id" exact={true} element={<UpdateForm />} />
+        </Routes>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
